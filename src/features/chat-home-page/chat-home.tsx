19b1b74137f2d71163026756f9f2c486a323d592
Astora-Consulting <142787044+Astora-Consulting@users.noreply.github.com>
2025-10-02 22:44:04 +0200

    Update chat-home.tsx

--- src/features/chat-home-page/chat-home.tsx
+++ src/features/chat-home-page/chat-home.tsx
@@ -22,11 +22,11 @@ export const ChatHome: FC<ChatPersonaProps> = (props) => {
           title={
             <>
               <Image
-                src={"/ai-icon.png"}
+                src={"/favicon.ico"}
                 width={60}
                 height={60}
                 quality={100}
-                alt="ai-icon"
+                alt="fav-icon"
               />{" "}
               {AI_NAME}
             </>
